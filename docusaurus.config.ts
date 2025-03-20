a276ca47a205df764e04b69db3d4ae2b1ad84744
JingpuRen <yy5396681@qq.com>
2025-03-20 22:50:43 +0800

    修改右上角Github的跳转网址

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -82,7 +82,7 @@ const config: Config = {
         },
         {to: '/blog', label: 'Blog', position: 'left'},
         {
-          href: 'https://github.com/facebook/docusaurus',
+          href: 'https://github.com/JingpuRen/my-website',
           label: 'GitHub',
           position: 'right',
         },
