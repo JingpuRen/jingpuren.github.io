e90dd3a633ed327a5448c949ead672ade9bae97d
JingpuRen <yy5396681@163.com>
2025-03-21 22:36:54 +0800

    增添配置文件中的projectName

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  projectName: 'JingpuRen.github.io', // Usually your repo name.
+  projectName: 'jingpu-website', // Usually your repo name.
   organizationName: 'JingpuRen', // Usually your GitHub org/user name.
   deploymentBranch: 'gh-pages',
   trailingSlash: false,
