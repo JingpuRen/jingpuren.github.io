b8f1bebdb5c3edb6d3f097d3b09ff77c243837d3
JingpuRen <yy5396681@163.com>
2025-03-22 12:36:41 +0800

    修改开始页的url标题

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -38,7 +38,8 @@ export default function Home(): ReactNode {
     const {siteConfig} = useDocusaurusContext();
     return (
         <Layout
-      title={`Hello from ${siteConfig.title}`}
+      // title={`Hello from ${siteConfig.title}`}
+      title={`${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
